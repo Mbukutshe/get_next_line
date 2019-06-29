0e7fc873a9d40eacc008422997de593d0c468734
Mbukutshe <kmbukuts@student.wethinkcode.co.za>
2019-06-29 15:11:25 +0200

    Add files via upload

--- Makefile
+++ Makefile
@@ -3,23 +3,27 @@
 #                                                         :::      ::::::::    #
 #    Makefile                                           :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: cletinic <marvin@42.fr>                    +#+  +:+       +#+         #
+#    By: kmbukuts <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2018/05/24 11:04:12 by cletinic          #+#    #+#              #
-#    Updated: 2018/06/08 13:38:04 by cletinic         ###   ########.fr        #
+#    Created: 2019/06/20 08:15:23 by kmbukuts          #+#    #+#              #
+#    Updated: 2019/06/28 16:11:41 by kmbukuts         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-CC = gcc
 NAME = test_gnl
-FLAG = -Wall -Wextra -Werror
+
+CC = gcc
+
 SRC = main.c get_next_line.c
+
 OBJ = $(SRC:.c=.o)
+
+FLAGS = -Wall -Werror -Wextra
 
 $(NAME):
 	gcc -c -Wall -Wextra -Werror $(SRC)
 	make -C libft/ fclean && make -C libft/
-	clang $(FLAG) -I libft/ -c $(SRC)
+	clang $(FLAGS) -I libft/ -c $(SRC)
 	clang -o $(NAME) $(OBJ) -I libft/ -L libft/ -lft
 
 all: $(NAME)
@@ -30,9 +34,9 @@ clean:
 fclean: clean
 	/bin/rm -f $(NAME)
 
-re:	fclean all
+re: fclean all
 
 #%.o: %.c
-#	$(CC) $(FLAG) -c $< -o $@
+#   $(CC) $(FLAG) -c $< -o $@
 
 .PHONY: all clean fclean re
--- get_next_line.c
+++ get_next_line.c
@@ -1,70 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmbukuts <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/20 07:47:26 by kmbukuts          #+#    #+#             */
+/*   Updated: 2019/06/29 15:10:18 by kmbukuts         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static int		ft_getbuffer(const int fd, char **line)
+static int	ft_get_line(const int fd, char **line)
 {
-	char		*buffer;
-	char		*temp;
-	int			value;
+	char	*buffer;
+	char	*ptr;
+	int		i;
 
 	if ((buffer = (char *)malloc(sizeof(*buffer) * (BUFF_SIZE + 1))) == NULL)
 		return (-1);
-
-	value = read(fd, buffer, BUFF_SIZE);
-
-	if (value > 0)
+	i = read(fd, buffer, BUFF_SIZE);
+	if (i > 0)
 	{
-		buffer[value] = '\0';
-		temp = ft_strjoin(*line, buffer);
+		buffer[i] = '\0';
+		ptr = ft_strjoin(*line, buffer);
 		free(*line);
-		*line = temp;
+		*line = ptr;
 	}
 	free(buffer);
-	return (value);
+	return (i);
 }
 
-static int	read_and_join(const int fd, char **stack, char **line_feed)
+static int	ft_put_cont(const int fd, char **cont, char **line)
 {
-	int			bytes;
+	int	i;
 
-	while (*line_feed == '\0')
+	while (*line == '\0')
 	{
-		bytes = ft_getbuffer(fd, *&stack);
-		if (bytes == 0)
+		i = ft_get_line(fd, cont);
+		if (i == 0)
 		{
-			if (ft_strlen(*stack) == 0)
+			if (ft_strlen(*cont) == 0)
 				return (0);
-			*stack = ft_strjoin(*stack, "\n");
+			*cont = ft_strjoin(*cont, "\n");
 		}
-		if (bytes < 0)
+		if (i < 0)
 			return (-1);
 		else
-			*line_feed = ft_strchr(*stack, '\n');
+			*line = ft_strchr(*cont, '\n');
 	}
 	return (1);
 }
 
-int				get_next_line(const int fd, char **line)
+int			get_next_line(const int fd, char **line)
 {
-	static char	*stack = NULL;
-	char		*temp;
-	int			ret;
-	char		*line_feed;
-
-	if ((!stack && (stack = (char*)malloc(sizeof(*stack))) == NULL) || !line
-			|| fd < 0 || BUFF_SIZE < 0)
+	static char		*stack = NULL;
+	char			*ln;
+	char			*ptr;
+	int				i;
+	
+	if (!stack && (stack = (char *)malloc(sizeof(*stack))) == NULL)
 		return (-1);
-	line_feed = ft_strchr(stack, '\n');
-	ret = read_and_join(fd, &stack, &line_feed);
-	if (ret == 0 || ret == -1)
+	if (!line || fd < 0 || BUFF_SIZE < 0)
+		return (-1);
+	ln = ft_strchr(stack, '\n');
+	i = ft_put_cont(fd, &stack, &ln);
+	if (i == 0 || i == -1)
 	{
-		if (ret == 0)
+		if (i == 0)
 			*line = ft_strdup("");
-		return (ret);
+		return (i);
 	}
-	*line = ft_strsub(stack, 0, ft_strlen(stack) - ft_strlen(line_feed));
-	temp = ft_strdup(line_feed + 1);
+	*line = ft_strsub(stack, 0, ft_strlen(stack) - ft_strlen(ln));
+	ptr = ft_strdup(ln + 1);
 	ft_strdel(&stack);
-	stack = temp;
-	return (ret);
+	stack = ptr;
+	return (i);
 }
